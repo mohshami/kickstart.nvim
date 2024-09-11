444eeb114160abdd3cb01cfcdf575ef63a3066b3
Mohammad AlShami <mohammad.alshami@cira.ca>
2024-09-10 21:53:19 -0400

    Add options and keymaps

--- lua/custom/config/keymaps.lua
+++ lua/custom/config/keymaps.lua
@@ -1,1 +1,11 @@
 vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
+vim.keymap.set({ 'n', 'v' }, '<leader>p', [["+p]])
+vim.keymap.set({ 'n', 'v' }, '<leader>P', [["+P]])
+
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+vim.keymap.set('n', '<leader>Y', [["+Y]])
+
+-- Stay in indent mode
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
--- lua/custom/config/options.lua
+++ lua/custom/config/options.lua
@@ -1,3 +1,11 @@
 vim.opt.relativenumber = true
 vim.opt.clipboard = ''
 vim.g.have_nerd_font = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.colorcolumn = '80'
+vim.opt.cursorline = true
