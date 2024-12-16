06b55b631c29de75a2bf213460d84a992bcd050d
Mohammad H. AlShami <mohshami@gmail.com>
2024-12-16 12:51:05 -0500

    For nix files, set indentation

--- lua/custom/config/options.lua
+++ lua/custom/config/options.lua
@@ -1,5 +1,6 @@
 vim.opt.relativenumber = true
 vim.opt.clipboard = ''
+-- This needs to be set in init.lua
 vim.g.have_nerd_font = true
 
 vim.opt.tabstop = 4
@@ -9,3 +10,11 @@ vim.opt.expandtab = true
 
 vim.opt.colorcolumn = '80'
 vim.opt.cursorline = true
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "nix",
+    callback = function()
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.tabstop = 2
+    end
+})
