549ed04678b91efd80c7efff1443e160d30d31d3
Mohammad H. AlShami <mohshami@gmail.com>
2024-12-16 12:50:49 -0500

    Enable nerd font

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -3,5 +3,5 @@ return {
   opts = {},
   -- Optional dependencies
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 }
