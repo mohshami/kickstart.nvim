60c95c791b16116969cab7e6eaeb5d101dad614f
Mohammad AlShami <mohshami@gmail.com>
2024-12-17 04:03:36 -0500

    be explicit about changing the system clipboard

--- lua/custom/config/options.lua
+++ lua/custom/config/options.lua
@@ -1,5 +1,7 @@
 vim.opt.relativenumber = true
-vim.opt.clipboard = ''
+vim.schedule(function()
+  vim.opt.clipboard = ''
+end)
 -- This needs to be set in init.lua
 vim.g.have_nerd_font = true
 
@@ -11,10 +13,10 @@ vim.opt.expandtab = true
 vim.opt.colorcolumn = '80'
 vim.opt.cursorline = true
 
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "nix",
-    callback = function()
-        vim.opt_local.shiftwidth = 2
-        vim.opt_local.tabstop = 2
-    end
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'nix',
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+  end,
 })
